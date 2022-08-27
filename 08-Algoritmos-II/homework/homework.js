e64d4e26fbafb57495939b8a4e6c4b9eda226cf4
Kalusa93 <lucaslegui21@gmail.com>
2022-08-27 13:11:42 -0300

    homework 8

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,18 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if(array.length <= 1) {
+    return array;
+  }
+
+  var pivot = array[0];
+  var left = [];
+  var right = [];
+
+  for(var i = 1; i < array.length; i++) {
+    array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +26,25 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  function merge(left, right) {
+    let sortedArr = [];
+    while(left.length && right.length) {
+      if(left[0] < right[0]) {
+        sortedArr.push(left.shift());
+      }
+      else {
+        sortedArr.push(right.shift());
+      }
+    }
+    return [...sortedArr, ...left, ...right];
+  }
+  const half = array.length/2;
+  if(array.length <= 1) {
+    return array;
+  }
+  const left = array.splice(0, half);
+  const right = array;
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 // No modificar nada debajo de esta línea
