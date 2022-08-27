7baaac982a9d4a74aa203e8a8d9ba8cfb4164ee6
Kalusa93 <lucaslegui21@gmail.com>
2022-08-27 12:11:32 -0300

    homework 7

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,15 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  var divisores = [1];
+  for(let i = 2; i <= num; i++) {
+    if (num % i === 0) {
+      divisores.push(i);
+      num = num/i;
+      i--;
+    }
+  }
+  return divisores;
 }
 
 function bubbleSort(array) {
@@ -14,7 +23,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array.length; j++) {
+      if(array[j] > array[j+1]) {
+        var mayor = array[j];
+        array[j] = array[j+1]
+        array[j+1] = mayor;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +41,18 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+    for (let i = 1; i < array.length; i++) {
+      // Choosing the first element in our unsorted subarray
+      let current = array[i];
+      // The last element of our sorted subarray
+      let j = i-1; 
+      while ((j > -1) && (current < array[j])) {
+        array[j+1] = array[j];
+        j--;
+      }
+      array[j+1] = current;
+    }
+  return array;
 }
 
 
@@ -32,7 +61,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 0; i < array.length; i++) {
+    var min = i;
+    for(let j = i+1; j < array.length; j++) {
+      if(array[j] < array[min]) {
+        min = j;
+      }
+    }
+    var temp = array[i];
+    array[i] = array[min];
+    array[min] = temp;
+  }
+    return array;
 }
 
 
