d8cae5c3d417bcf63ac528d5ca6b634f48040e0f
Kalusa93 <lucaslegui21@gmail.com>
2022-08-27 12:26:13 -0300

    homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var sum = 0;
+    for (let i = 0; i < num.length; i++) {
+       sum = sum + num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var bin = [];
+  while(num > 0) {
+    bin.push(num % 2);
+    num = Math.floor(num/2);
+  }
+  bin = bin.reverse().join('');
+  return bin;
 }
 
 
